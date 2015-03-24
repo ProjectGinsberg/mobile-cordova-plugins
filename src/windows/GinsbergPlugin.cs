43c94dde571b82426eb1a9c973c8bb29456430b6
Workings <cpdavies@gmail.com>
2015-03-24 15:41:11 +0000

    Trying cs plugin

---
+++ src/windows/GinsbergPlugin.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace WPCordovaClassLib.Cordova.Commands
+{
+    public class GinsbergPlugin : BaseCommand
+    {
+        public void showy(string truth)
+        {
+			Console.Write("we3f");
+			/*
+            string upperCase = JSON.JsonHelper.Deserialize<string[]>(options)[0].ToUpper();
+            PluginResult result;
+            if (upperCase != "")
+            {
+                result = new PluginResult(PluginResult.Status.OK, upperCase);
+            } else
+            {
+                result = new PluginResult(PluginResult.Status.ERROR, upperCase);
+            }
+
+            DispatchCommandResult(result);
+			*/
+        }
+    }
+}
