b7c76f8cd464e33a176439e9d21c809923fa8e90
Workings <cpdavies@gmail.com>
2015-03-24 15:01:29 +0000

    Testing

--- www/ginsbergplugin.js
+++ www/ginsbergplugin.js
@@ -1,17 +1,7 @@
-var exec = require('cordova/exec');
-
-module.exports = {
-
-	show : function(title, message, cancelCallback) {
-        if (cancelCallback == true && typeof cancelCallback !== "function") {
-            cancelCallback = function () {};  
-        }
-        cordova.exec(cancelCallback, null, 'GinsbergPlugin', 'show', [ title, message, !!cancelCallback ]);
-    },
-
-
-    hide : function(success, fail) {
-        cordova.exec(success, fail, 'GinsbergPlugin', 'hide', [ "","" ]);
+var GinsbergPlugin = {
+    show: function (successCallback, errorCallback, strInput) {
+        cordova.exec(successCallback, errorCallback, "GinsbergPlugin", "show", [strInput]);
     }
+}
 
-};
+module.exports = GinsbergPlugin;
